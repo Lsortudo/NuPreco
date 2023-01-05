5fbfdd26b5725a8f6379caa9a2bd7f0c6a03ac19
L <leonardo.alvirubro@hotmail.com>
2023-01-05 11:11:40 -0300

    perf: Utils created { for now only validator/constants }

---
+++ app/src/main/java/com/example/nupreco/utils/constants.kt
@@ -0,0 +1,4 @@
+package com.example.nupreco.utils
+
+const val DATABASE_NAME_INGREDIENT = "ingredient_database"
+const val DATABASE_NAME_RECIPE = "recipe_database"
---
+++ app/src/main/java/com/example/nupreco/utils/validatorRecipe.kt
@@ -0,0 +1,4 @@
+package com.example.nupreco.utils
+
+class validatorRecipe {
+}
