4240607c350233b651f09f3e92a12ef175498f1b
L <leonardo.alvirubro@hotmail.com>
2023-01-07 14:49:54 -0300

    feat: IngredientViewModel basic implementation

--- app/src/main/java/com/example/nupreco/viewmodel/IngredientViewModel.kt
+++ app/src/main/java/com/example/nupreco/viewmodel/IngredientViewModel.kt
@@ -1,15 +1,39 @@
 package com.example.nupreco.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.nupreco.models.Ingredient
+import com.example.nupreco.repositories.NuPrecoRepository
+import kotlinx.coroutines.launch
 
-class IngredientViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        /*value = "This is dashboard Fragment"*/
+class IngredientViewModel(private val repository: NuPrecoRepository) : ViewModel() {
+    // Gerado automaticamente
+    /*private val _text = MutableLiveData<String>().apply {
+        *//*value = "This is dashboard Fragment"*//*
         value = "Pagina de ingredientes"
 
     }
-    val text: LiveData<String> = _text
+    val text: LiveData<String> = _text*/
+
+    val allIngredients: LiveData<List<Ingredient>> =
+        repository.allIngredients.asLiveData()
+
+    fun insert(ingredient: Ingredient) =
+        viewModelScope.launch {
+            //Insertions on database should ALWAYS be on ANY thread besides the main one (using coroutines inside my viewModelScope)
+            repository.insertIngredient(ingredient)
+        }
+}
+
+class IngredientViewModelFactory(private val repository: NuPrecoRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(IngredientViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return IngredientViewModel(repository) as T
+            //return super.create(modelClass)
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+
+    }
+
 }
